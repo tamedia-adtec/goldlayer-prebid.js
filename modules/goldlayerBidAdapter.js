773d59b262eb3c7681318a1033c329e2730ceec7
Ben Brachmann <49547103+bevenio@users.noreply.github.com>
2024-11-06 16:35:48 +0100

    first request (WIP)

--- modules/goldlayerBidAdapter.js
+++ modules/goldlayerBidAdapter.js
@@ -6,24 +6,97 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 /* Constants */
 const BIDDER_CODE = 'goldlayer';
 const GVLID = 580;
-// const URL = 'https://goldlayer-api.prod.gbads.net/bid/prebidjs';
+const URL = 'https://goldlayer-api.prod.gbads.net/bid';
 
-/* Custom business logic */
+/* Mapping */
+const convertToProprietaryData = (validBidRequests, bidderRequest) => {
+  const requestData = {
+    mock: false,
+    debug: true,
+    config: {
+      publisher: {
+        id: undefined,
+      }
+    },
+    gdpr: {
+      consent: undefined,
+      consentString: undefined,
+    },
+    contextInfo: {
+      contentUrl: undefined,
+      bidderResources: undefined,
+    },
+    appInfo: {
+      id: undefined,
+    },
+    userInfo: {
+      ip: undefined,
+      ua: undefined,
+      ifa: undefined,
+      ppid: [],
+    },
+    slots: [],
+    targetings: {},
+  };
+
+  // Set config
+  if (validBidRequests[0]?.params?.publisherId) {
+    requestData.config.publisher.id = validBidRequests[0].params.publisherId
+  }
+
+  // Set GDPR
+  if (bidderRequest?.gdprConsent) {
+    requestData.gdpr.consent = bidderRequest.gdprConsent.gdprApplies;
+    requestData.gdpr.consentString = bidderRequest.gdprConsent.consentString;
+  }
+
+  // Set contextInfo
+  requestData.contextInfo.contentUrl = bidderRequest?.ortb2?.site?.page;
+
+  // Set appInfo
+  requestData.appInfo.id = bidderRequest?.ortb2?.site?.domain;
+
+  // Set userInfo
+  requestData.userInfo.ip = undefined;
+  requestData.userInfo.ifa = undefined;
+  requestData.userInfo.ppid = [];
+  requestData.userInfo.ua = bidderRequest?.ortb2?.device?.ua;
+
+  // Set slots
+  requestData.slots = validBidRequests.map((bid) => {
+    const slot = {
+      id: bid?.adUnitCode,
+      sizes: bid?.sizes,
+      targetings: bid?.params?.customTargeting,
+    };
+    return slot;
+  });
+
+  return requestData;
+}
 
 export const spec = {
   code: BIDDER_CODE,
   gvlid: GVLID,
   supportedMediaTypes: [BANNER, VIDEO, NATIVE],
   isBidRequestValid: function (bid) {
-    // Check all parameters that can be remapped to GL 
-
-    // Check if the bid has all neccessary parameters for goldlayer
-    utils.logInfo('isBidRequestValid', bid);
-    return !!(bid.params.publisherId);
+    // Check for publisher environment id
+    return typeof bid.params.publisherId === 'string';
   },
   buildRequests: function (validBidRequests, bidderRequest) {
     // Transform prebidJS request into goldlayer request
     utils.logInfo('buildRequests', validBidRequests, bidderRequest);
+    const data = convertToProprietaryData(validBidRequests, bidderRequest);
+    return [{
+      method: 'POST',
+      url: URL,
+      data: data,
+      bidderRequest: bidderRequest,
+      options: {
+        withCredentials: false,
+        contentType: 'application/json',
+      }
+    }];
   },
   // Skip for now
   getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {},
